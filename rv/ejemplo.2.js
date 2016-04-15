13761b97a232c791c1966251af2c4ded381e759d
CoconoJC <jceh_cocono@hotmail.com>
2016-04-14 22:23:50 -0500

    Update ejemplo.2.js

--- rv/ejemplo.2.js
+++ rv/ejemplo.2.js
@@ -11,11 +11,11 @@ Wall.prototype = new THREE.Mesh();
 Environment.prototype.setMap = function(map) {
   var _offset = Math.floor(map.length/2);
   
-  for ( var i = 0; i < mapa.length ; i++)
-  for ( var j = 0; j < mapa.length ; j++) {
-    if(mapa[i][j]==="x")
+  for ( var i = 0; i < map.length ; i++)
+  for ( var j = 0; j < map.length ; j++) {
+    if(map[i][j]==="x")
     this.add(new Wall(1,j-_offset,-(i-_offset)));
-    else if(mapa[i][j]==='r')
+    else if(map[i][j]==='r')
     this.add(new Robot(0.5,j-_offset,-(i-_offset)));
   }
 }
